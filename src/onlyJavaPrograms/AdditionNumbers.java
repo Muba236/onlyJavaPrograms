a131cc9dcfb255833b0ac35b6edc189bce0f6494
unknown <mubaraksyeds236@gmail.com>
2025-02-16 17:40:43 +0530

    addition of 2 numbers

---
+++ src/onlyJavaPrograms/AdditionNumbers.java
@@ -0,0 +1,16 @@
+package onlyJavaPrograms;
+
+public class AdditionNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10, b = 20;
+		
+		int c = a + b;
+		
+		System.out.println(c);
+
+	}
+
+}
